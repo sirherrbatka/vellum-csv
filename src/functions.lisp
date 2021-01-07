da947a3a354cbb75220e0ab3f6be13fb2fcef9a7
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-01-07 16:39:22 +0100

    Added option to skip the header.

--- src/functions.lisp
+++ src/functions.lisp
@@ -1,9 +1,12 @@
 (cl:in-package #:vellum-csv)
 
 
-(defun csv-range (path/range)
+(defun csv-range (path/range &key includes-header-p)
   (make-instance
    'csv-range :original-range (if (or (stringp path/range)
                                       (pathnamep path/range))
-                                  (cl-ds.fs:line-by-line path/range)
-                                  path/range)))
+                                  (~> (cl-ds.fs:line-by-line path/range)
+                                      (cl-ds:drop-front (if includes-header-p 1 0))
+                                      cl-ds:clone)
+                                  path/range)
+              :header includes-header-p))
--- src/types.lisp
+++ src/types.lisp
@@ -2,4 +2,9 @@
 
 
 (defclass csv-range (cl-ds.alg:proxy-range)
-  ())
+  ((%header :initarg :header
+            :reader header)))
+
+
+(defmethod cl-ds.utils:cloning-information append ((object csv-range))
+  '((:header header)))
