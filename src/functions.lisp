7b4a208cabd7a9d6db34ad3cee987b814d469321
Marek Kochanowicz <sirherrbatka@gmail.com>
2021-11-11 20:59:06 +0100

    Added check-types on the csv-range constructor functions.

--- src/functions.lisp
+++ src/functions.lisp
@@ -2,6 +2,10 @@
 
 
 (defun csv-range (path &key includes-header-p header separator quote )
+  (check-type path (or string pathname cl-ds.fs:command))
+  (check-type header vellum.header:standard-header)
+  (check-type separator character)
+  (check-type quote character)
   (make-instance
    'csv-range :path path
               :header header
